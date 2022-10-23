682d6950487a16f735fe6a029b112168dcc09c37
galehriski <116035286+galehriski@users.noreply.github.com>
2022-10-23 18:37:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,22 +49,22 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 
-global.sig = 'https://instagram.com/_ctzhid' // ig
-global.sgh = 'https://github.com/WH-MODS-BOT/' // github
-global.sgc = 'https://chat.whatsapp.com/FQkbyTEfvES3D363uxMEFOI' // group whatsapp
-global.sdc = 'https://www.discord.com'//discord
-global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
-global.sfb = 'https://www.facebook.com/' // facebook
-global.syt = 'https://www.youtube.com/' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
+global.sig = 'https://instagram.com/gal3h_jr' // ig
+global.sgh = 'https://github.com/GANZ-BOT/' // github
+global.sgc = 'https://chat.whatsapp.com/BBfnFBnsg9AD0mZSnriGyK' // group whatsapp
+global.sdc = '-'//discord
+global.snh = 'https://youtube.com/channel/UCY_rcabCcFltXQWwCQlO03A' // my youtube channel
+global.sfb = '-' // facebook
+global.syt = '-' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
 
 /*============== PAYMENT ==============*/
-global.pdana = '08XXXXXXXXXX' //dana
-global.povo = '08XXXXXXXXXX' //ovo
-global.pgopay = '08XXXXXXXXXX' //gopay
-global.plinkaja = '08XXXXXXXXXX' //link aja
-global.ppulsa = '08XXXXXXXXXX' //telkomsel
-global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
-global.psaweria = 'https://saweria.co/mrw4h1d'
+global.pdana = '085664221805' //dana
+global.povo = '085664221805' //ovo
+global.pgopay = '085664221805' //gopay
+global.plinkaja = '085664221805' //link aja
+global.ppulsa = '085664221805' //indosat
+global.ppulsa2 = '-' // kalau ada kartu lain isi aja
+global.psaweria = 'https://saweria.co/crisper'
 
 /*============== NOMOR ==============*/
 global.nomorbot = '62XXXXXXXXXXX'
