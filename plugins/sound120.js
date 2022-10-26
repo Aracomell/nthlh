255559581e0710bf10dde96c9df65fc0102534d2
FlawFX <114559769+FlawFX@users.noreply.github.com>
2022-10-26 11:18:41 +0700

    Update sound120.js

--- plugins/sound120.js
+++ plugins/sound120.js
@@ -6,10 +6,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
 conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaUrl: "https://chat.whatsapp.com/KNkpoT4oX5yFA18GlPttgl",
     mediaType: 2,
     description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
-    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    title: '© ZennX-Botz',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig  }}})
