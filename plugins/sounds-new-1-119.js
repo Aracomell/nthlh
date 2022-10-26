e1a8aa6324340001a54a17383ce15b8c6bed004d
FlawFX <114559769+FlawFX@users.noreply.github.com>
2022-10-26 11:24:28 +0700

    Update sounds-new-1-119.js

--- plugins/sounds-new-1-119.js
+++ plugins/sounds-new-1-119.js
@@ -16,7 +16,7 @@ let stiker = await sticker(null, global.API(`https://telegra.ph/file/d34b2ab2cb2
           showAdAttribution: true,
     mediaUrl: sig,
     mediaType: 2,
-    description: 'WH MODS BOT MD 𝑿 WH MODS DEV', 
+    description: '© ZennX-Botz', 
     title: `${command} Sedang Di Proses`,
     body: botdate,
     thumbnail: await(await fetch(pp)).buffer(),
@@ -30,13 +30,13 @@ await conn.sendFile(m.chat, audio, 'error.mp3', null, fkontak, true, {
 type: 'audioMessage', 
 ptt: false, seconds: 0,contextInfo: {
          externalAdReply: { showAdAttribution: true,
- mediaUrl: 'www.instagram.com/_ctzhid',
+ mediaUrl: 'https://chat.whatsapp.com/KNkpoT4oX5yFA18GlPttgl',
     mediaType: 2, 
-    description: 'www.instagram.com/_ctzhid',
+    description: 'www.instagram.com/karfuanyogich',
     title: "Now Playing...",
     body: wm,
-    thumbnail: await (await fetch('https://i.ibb.co/XZrK6yQ/transformers.jpg')).buffer(),
-    sourceUrl: 'www.instagram.com/_ctzhid'
+    thumbnail: await (await fetch('https://telegra.ph/file/1860e660b4c9d465b49dc.jpg')).buffer(),
+    sourceUrl: 'www.instagram.com/karfuanyogich'
  	  }
      }
     })
